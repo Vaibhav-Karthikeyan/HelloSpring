4d7e2465a4fc7a0038ad5bfa870a34df01bb8e9e
Vaibhav Karthikeyan <73402414+Vaibhav-Karthikeyan@users.noreply.github.com>
2023-01-15 16:47:37 +0530

    Add files via upload

---
+++ src/main/java/com/masai/app/Controller.java
@@ -0,0 +1,12 @@
+package com.masai.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello world";
+	}
+}
